3ab98384542a69cdadc47601e80ad4a855e58926
OlegZakharov <varahas@mail.ru>
2022-07-04 21:48:37 +0300

    add if else

---
+++ Example005_ifelse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter username");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Маша")
+{
+    Console.WriteLine("Hello, stranger");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
